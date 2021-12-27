f27aa7e42a4027336e0845dbf379ca15d528edc6
Kim Hyeonyeong <hggew@naver.com>
2021-12-27 23:39:14 +0900

    init SE_AL

---
+++ SE_AL/app/src/main/java/com/example/se_al/ui/classList/ClassListFragment.kt
@@ -0,0 +1,44 @@
+package com.example.se_al.ui.classList
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.se_al.databinding.FragmentClassListBinding
+
+class ClassListFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: ClassListViewModel
+    private var _binding: FragmentClassListBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProvider(this).get(ClassListViewModel::class.java)
+
+        _binding = FragmentClassListBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
