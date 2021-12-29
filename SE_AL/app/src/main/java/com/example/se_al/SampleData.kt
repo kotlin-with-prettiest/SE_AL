a880e48b8dfaf1c543b4006134da0082b591820d
Kim Hyeonyeong <hggew@naver.com>
2021-12-29 19:42:12 +0900

    feat : add recyclerview adapter

---
+++ SE_AL/app/src/main/java/com/example/se_al/SampleData.kt
@@ -0,0 +1,9 @@
+package com.example.se_al
+
+data class SampleData(
+    val name: String,
+    val date: String,
+    val title: String,
+    val memo: String,
+
+    )
--- SE_AL/app/src/main/java/com/example/se_al/ui/AlarmBaseListFragment.kt
+++ SE_AL/app/src/main/java/com/example/se_al/ui/AlarmBaseListFragment.kt
@@ -5,8 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.example.se_al.R
+import androidx.fragment.app.activityViewModels
+import com.example.se_al.SampleData
+import com.example.se_al.adapter.AlarmBaseListAdapter
 import com.example.se_al.databinding.FragmentAlarmBaseListBinding
 
 class AlarmBaseListFragment  : Fragment() {
@@ -14,6 +15,21 @@ class AlarmBaseListFragment  : Fragment() {
     private var _binding: FragmentAlarmBaseListBinding? = null
 
     private val binding get() = _binding!!
+
+    private val viewModel : AlarmBaseListViewModel by activityViewModels()
+
+        var sampleData = arrayListOf<SampleData>(
+        SampleData("sample1","2021-12-29","title1","memo1"),
+        SampleData("sample2","2021-12-30","title2","memo2"),
+        SampleData("sample3","2021-12-31","title3","memo3")
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        val mAdapter = AlarmBaseListAdapter(this,sampleData)
+//        binding.recyclerAlarmItem.adapter =mAdapter
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,12 +40,19 @@ class AlarmBaseListFragment  : Fragment() {
         _binding = FragmentAlarmBaseListBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        binding.recyclerAlarmItem.setOnClickListener{
-            findNavController().navigate(R.id.action_alarmBaseListFragment_to_alarmFixFragment)
-        }
+//        binding.recyclerAlarmItem.setOnClickListener{
+//            findNavController().navigate(R.id.action_alarmBaseListFragment_to_alarmFixFragment)
+//        }
 
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
     }
 
     override fun onDestroyView() {
