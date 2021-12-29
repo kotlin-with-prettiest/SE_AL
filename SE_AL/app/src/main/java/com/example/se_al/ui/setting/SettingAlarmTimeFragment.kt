2432ea85dc48f108e288f5281609d553f8d8b9b4
RightHennessy <myheaven_b@naver.com>
2021-12-29 21:28:41 +0900

    feat : add setting

---
+++ SE_AL/app/src/main/java/com/example/se_al/ui/setting/SettingAlarmTimeFragment.kt
@@ -0,0 +1,35 @@
+package com.example.se_al.ui.setting
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.se_al.R
+import com.example.se_al.databinding.FragmentClassBaseBinding
+
+
+
+class SettingAlarmTimeFragment : Fragment() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting_alarm_time, container, false)
+    }
+
+    companion object{
+
+    }
+
+
+}
