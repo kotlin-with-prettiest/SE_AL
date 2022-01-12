e197b95204c130430046442d2f343ee11311788b
Kim Hyeonyeong <hggew@naver.com>
2022-01-12 23:16:52 +0900

    chore : login fragment design

--- SE_AL/app/src/main/java/com/example/se_al/ui/setting/SettingPersonalInfoFragment.kt
+++ SE_AL/app/src/main/java/com/example/se_al/ui/setting/SettingPersonalInfoFragment.kt
@@ -2,12 +2,10 @@ package com.example.se_al.ui.setting
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.se_al.MainActivity
 import com.example.se_al.databinding.FragmentSettingPersonalInfoBinding
 import com.example.se_al.login.LoginActivity
 
@@ -29,7 +27,6 @@ class SettingPersonalInfoFragment : Fragment() {
 
         //로그아웃 버튼
         binding.btnLogout.setOnClickListener {
-            Log.d("Setting", "logout")
             val intent = Intent(this@SettingPersonalInfoFragment.context, LoginActivity::class.java)
             startActivity(intent)
             activity?.finish()
