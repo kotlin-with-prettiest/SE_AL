0aa86bc2df72151fda0f84d49f382d26a8bfde07
RightHennessy <myheaven_b@naver.com>
2022-01-13 19:23:13 +0900

    test : connect

--- SE_AL/app/src/main/java/com/example/se_al/ui/setting/SettingAlarmTimeFragment.kt
+++ SE_AL/app/src/main/java/com/example/se_al/ui/setting/SettingAlarmTimeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.se_al.R
@@ -26,6 +27,8 @@ class SettingAlarmTimeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_setting_alarm_time, container, false)
     }
+
+
 
     companion object{
 
