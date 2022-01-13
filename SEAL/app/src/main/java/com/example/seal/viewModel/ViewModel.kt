da7c71309eccbe888b7fd5f966ef650189fc947c
hggew <40794341+hggew@users.noreply.github.com>
2022-01-13 20:17:12 +0900

    Delete SEAL directory

--- SEAL/app/src/main/java/com/example/seal/viewModel/ViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.seal.viewModel
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.navigation.NavController
-
-class ViewModel : ViewModel() {
-    val currentNavController = MutableLiveData<NavController?>()
-}
