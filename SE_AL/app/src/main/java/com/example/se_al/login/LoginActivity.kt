bcd629652221c64d0a98e899edd6d786f9ec2fad
sally2596@gmail.com <sally2596@gmail.com>
2022-01-19 16:50:08 +0900

    fix: Error in FirstLogin API

--- SE_AL/app/src/main/java/com/example/se_al/login/LoginActivity.kt
+++ SE_AL/app/src/main/java/com/example/se_al/login/LoginActivity.kt
@@ -8,6 +8,7 @@ import com.example.se_al.MainActivity
 import com.example.se_al.R
 import com.example.se_al.databinding.ActivityLoginBinding
 import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.doAsync
 
 class LoginActivity: AppCompatActivity() {
 
@@ -26,12 +27,13 @@ class LoginActivity: AppCompatActivity() {
             // 계정 정보(아이디, 비번)는 LoginUIS 에서 직접 가져와서 쓰기
 
             //참이면 로그인 성공한거
-            if(firstBlackBoardLogin()){
-                Log.d("로그인","성공")
-            }
-            else{//로그인 실패 처리
-                Log.d("로그인","실패")
-            }
+           doAsync {
+           if(firstBlackBoardLogin()){
+               Log.d("로그인","성공")
+           }
+           else{//로그인 실패 처리
+               Log.d("로그인","실패")
+           }}
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
